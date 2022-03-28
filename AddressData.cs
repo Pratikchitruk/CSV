f6133a125c01a4913b35772db528b4ed1268a48c
Pratikchitruk <99415272+Pratikchitruk@users.noreply.github.com>
2022-03-28 20:09:37 +0530

    Push for CSV

---
+++ AddressData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVFile
+{
+    public class AddressData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Emai { get; set; }
+        public int PhoneNo { get; set; }
+        public string city { get; set; }
+        public int state { get; set; }
+        public string country { get; set; }
+
+    }
+}
---
+++ CSVHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+
+
+
+var csvConfig = new CsvConfiguration(CultureInfo.CurrentCulture)
+{
+    HasHeaderRecord = false
+};
+
+string ImportFilePath = @"C:\Users\Admin\OneDrive\Desktop\CSV\CSVFile\User.csv";
+
+using var streamReader = new StreamReader(ImportFilePath);
+using var csvReader = new CsvReader(streamReader, csvConfig);
+
+string value;
+while (csvReader.Read())
+{
+    for (int i = 0; csvReader.TryGetField<string>(i, out value); i++)
+    {
+        Console.Write($"{value} ");
+    }
+
+    Console.WriteLine();
+}
+
+
+
